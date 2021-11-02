6f5796727d7c0e797e6f4c7211f0f757ab31982b
scott-code <scottyschoolsuccess@gmail.com>
2021-11-02 20:31:01 +0100

    site_web

---
+++ javascript folder/index.js
@@ -0,0 +1,10 @@
+//AOS.init();
+
+let toogle = document.querySelector(".toogle");
+
+let body = document.querySelector("body");
+
+toogle.addEventListener("click", function () {
+  body.classList.toggle("open");
+
+});
